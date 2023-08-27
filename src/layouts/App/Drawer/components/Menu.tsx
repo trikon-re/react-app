8bd657ce2eaeba2d9641c9269e125ed11c0fc8fc
tam11a <ibrahimsadiktamim@gmail.com>
2023-08-28 05:05:20 +0600

    fixed: drawer

--- src/layouts/App/Drawer/components/Menu.tsx
+++ src/layouts/App/Drawer/components/Menu.tsx
@@ -1,297 +1,320 @@
 import React from "react";
 import { Icon } from "@iconify/react";
 import { Menu as AntMenu, MenuProps } from "antd";
-import { useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 
 type MenuItem = Required<MenuProps>["items"][number];
 
 function getItem(
-  label: React.ReactNode,
-  key: React.Key,
-  icon?: React.ReactNode,
-  children?: MenuItem[]
+	label: React.ReactNode,
+	key: React.Key,
+	icon?: React.ReactNode,
+	children?: MenuItem[],
+	disabled?: boolean
 ): MenuItem {
-  return {
-    key,
-    icon,
-    children,
-    label,
-  } as MenuItem;
+	return {
+		key,
+		icon,
+		children,
+		label,
+		disabled,
+	} as MenuItem;
 }
 
 const items: MenuItem[] = [
-  {
-    type: "group",
-    label: "Overview",
-    children: [
-      getItem("Dashboard", "/app", <Icon icon={"radix-icons:dashboard"} />),
-      getItem(
-        "My Leads",
-        "/app/leads/my-leads",
-        <Icon icon={"mdi:funnel-check-outline"} />
-      ),
-    ],
-  },
-  {
-    type: "group",
-    label: "Operations",
-    children: [
-      getItem("Leads", "/leads", <Icon icon={"iconamoon:funnel-light"} />, [
-        getItem(
-          "Overview",
-          "/app/leads/overview",
-          <Icon icon={"icon-park-outline:view-grid-list"} />
-        ),
-        getItem(
-          "Create New Lead",
-          "/app/leads/create",
-          <Icon icon={"mdi:funnel-plus-outline"} />
-        ),
-        getItem(
-          "List of Leads",
-          "/app/leads/all",
-          <Icon icon={"fluent:data-funnel-24-regular"} />,
-          [
-            getItem(
-              "All Leads",
-              "/app/leads/all",
-              <Icon icon={"mdi:funnel-multiple-outline"} />
-            ),
-            getItem(
-              "New Leads",
-              "/app/leads/all?assign=null&total_assigned_persons=0",
-              <Icon icon={"ic:outline-new-releases"} />
-            ),
-            getItem(
-              "Pending Leads",
-              "/app/leads/all?status=pending",
-              <Icon icon={"ri:rest-time-line"} />
-            ),
-            getItem(
-              "Complete Leads",
-              "/app/leads/all?status=done",
-              <Icon icon={"mdi:funnel-check-outline"} />
-            ),
-            getItem(
-              "Canceled Leads",
-              "/app/leads/all?status=canceled",
-              <Icon icon={"mdi:funnel-remove-outline"} />
-            ),
-          ]
-        ),
-        getItem("Trash", "/leads/trash", <Icon icon={"gg:trash"} />),
-        getItem(
-          "Reports",
-          "/app/leads/reports",
-          <Icon icon={"tabler:report"} />,
-          []
-        ),
-      ]),
-      getItem("Properties", "/properties", <Icon icon={"mdi:building"} />, [
-        getItem(
-          "Overview",
-          "/app/properties/overview",
-          <Icon icon={"icon-park-outline:view-grid-list"} />
-        ),
-        getItem(
-          "Create New Property",
-          "/app/properties/create",
-          <Icon icon={"bi:building-add"} />
-        ),
-        getItem(
-          "List of Properties",
-          "/app/properties/all",
-          <Icon icon={"fluent:data-funnel-24-regular"} />,
-          [
-            getItem(
-              "All Properties",
-              "/app/properties",
-              <Icon icon={"bx:buildings"} />
-            ),
-            getItem(
-              "New Properties",
-              "/app/properties/all?date=last-7-days",
-              <Icon icon={"ic:outline-new-releases"} />
-            ),
-            getItem(
-              "Available Properties",
-              "/app/properties/all?status=available",
-              <Icon icon={"bi:building"} />
-            ),
-            getItem(
-              "Sold Properties",
-              "/app/properties/all?status=sold",
-              <Icon icon={"bi:building-check"} />
-            ),
-            getItem(
-              "Canceled Properties",
-              "/app/properties/all?status=canceled",
-              <Icon icon={"bi:building-x"} />
-            ),
-          ]
-        ),
-        getItem("Trash", "/properties/trash", <Icon icon={"gg:trash"} />),
-        getItem(
-          "Reports",
-          "/app/properties/reports",
-          <Icon icon={"tabler:report"} />,
-          []
-        ),
-      ]),
-      getItem("Media", "/media", <Icon icon={"radix-icons:dashboard"} />, [
-        getItem(
-          "Overview",
-          "/app/media/overview",
-          <Icon icon={"icon-park-outline:view-grid-list"} />
-        ),
-        getItem(
-          "Create New Media",
-          "/app/media/create",
-          <Icon icon={"radix-icons:dashboard"} />
-        ),
-        getItem(
-          "List of Media",
-          "/app/media/all",
-          <Icon icon={"radix-icons:dashboard"} />,
-          [
-            getItem(
-              "All Media",
-              "/app/media/all",
-              <Icon icon={"radix-icons:dashboard"} />
-            ),
-            getItem(
-              "Property Media",
-              "/app/media/all?linked_to=property",
-              <Icon icon={"radix-icons:dashboard"} />
-            ),
-            getItem(
-              "Lead Media",
-              "/app/media/all?linked_to=lead",
-              <Icon icon={"radix-icons:dashboard"} />
-            ),
-          ]
-        ),
-        getItem(
-          "Trash",
-          "/app/media/trash",
-          <Icon icon={"radix-icons:dashboard"} />
-        ),
-        getItem(
-          "Reports",
-          "/media/reports",
-          <Icon icon={"radix-icons:dashboard"} />,
-          []
-        ),
-      ]),
-    ],
-  },
-  {
-    type: "group",
-    label: "Organization",
-    children: [
-      getItem(
-        "Employees",
-        "/app/employee",
-        <Icon icon={"radix-icons:dashboard"} />,
-        [
-          getItem(
-            "Create New Employee",
-            "/app/create/employee",
-            <Icon icon={"radix-icons:dashboard"} />
-          ),
-          getItem(
-            "List of Employees",
-            "/app/employees/all",
-            <Icon icon={"radix-icons:dashboard"} />,
-            [
-              getItem(
-                "All Employees",
-                "/app/employees",
-                <Icon icon={"radix-icons:dashboard"} />
-              ),
-              getItem(
-                "New Employees",
-                "/app/employees/all?join_date<7d",
-                <Icon icon={"radix-icons:dashboard"} />
-              ),
-              getItem(
-                "Active Employees",
-                "/app/employees/all?active=true",
-                <Icon icon={"radix-icons:dashboard"} />
-              ),
-              getItem(
-                "Suspended Employees",
-                "/app/employees/all?is_active=false",
-                <Icon icon={"radix-icons:dashboard"} />
-              ),
-            ]
-          ),
-          getItem(
-            "Trash",
-            "/app/employees/trash",
-            <Icon icon={"radix-icons:dashboard"} />
-          ),
-        ]
-      ),
-      getItem("Roles", "/app/role", <Icon icon={"radix-icons:dashboard"} />, [
-        getItem(
-          "Create New Role",
-          "/app/create/role",
-          <Icon icon={"radix-icons:dashboard"} />
-        ),
-        getItem(
-          "List of Roles",
-          "/app/roles/all",
-          <Icon icon={"radix-icons:dashboard"} />,
-          [
-            getItem(
-              "All Roles",
-              "/app/roles",
-              <Icon icon={"radix-icons:dashboard"} />
-            ),
-            getItem(
-              "New Roles",
-              "/app/roles/all?join_date<7d",
-              <Icon icon={"radix-icons:dashboard"} />
-            ),
-            getItem(
-              "Active Roles",
-              "/app/roles/all?active=true",
-              <Icon icon={"radix-icons:dashboard"} />
-            ),
-            getItem(
-              "Suspended Roles",
-              "/app/roles/all?is_active=false",
-              <Icon icon={"radix-icons:dashboard"} />
-            ),
-          ]
-        ),
-        getItem(
-          "Trash",
-          "/app/roles/trash",
-          <Icon icon={"radix-icons:dashboard"} />
-        ),
-      ]),
-    ],
-  },
+	{
+		type: "group",
+		label: "Overview",
+		children: [
+			getItem("Dashboard", "/app", <Icon icon={"radix-icons:dashboard"} />),
+			getItem(
+				"My Leads",
+				"/app/leads/my-leads",
+				<Icon icon={"mdi:funnel-check-outline"} />,
+				undefined,
+				true
+			),
+		],
+	},
+	{
+		type: "group",
+		label: "Operations",
+		children: [
+			getItem(
+				"Leads",
+				"/leads",
+				<Icon icon={"iconamoon:funnel-light"} />,
+				[
+					getItem(
+						"Overview",
+						"/app/leads/overview",
+						<Icon icon={"icon-park-outline:view-grid-list"} />
+					),
+					getItem(
+						"Create New Lead",
+						"/app/leads/create",
+						<Icon icon={"mdi:funnel-plus-outline"} />
+					),
+					getItem(
+						"List of Leads",
+						"/app/leads/all",
+						<Icon icon={"fluent:data-funnel-24-regular"} />,
+						[
+							getItem(
+								"All Leads",
+								"/app/leads/all",
+								<Icon icon={"mdi:funnel-multiple-outline"} />
+							),
+							getItem(
+								"New Leads",
+								"/app/leads/all?assign=null&total_assigned_persons=0",
+								<Icon icon={"ic:outline-new-releases"} />
+							),
+							getItem(
+								"Pending Leads",
+								"/app/leads/all?status=pending",
+								<Icon icon={"ri:rest-time-line"} />
+							),
+							getItem(
+								"Complete Leads",
+								"/app/leads/all?status=done",
+								<Icon icon={"mdi:funnel-check-outline"} />
+							),
+							getItem(
+								"Canceled Leads",
+								"/app/leads/all?status=canceled",
+								<Icon icon={"mdi:funnel-remove-outline"} />
+							),
+						]
+					),
+					getItem("Trash", "/leads/trash", <Icon icon={"gg:trash"} />),
+					getItem(
+						"Reports",
+						"/app/leads/reports",
+						<Icon icon={"tabler:report"} />,
+						[]
+					),
+				],
+				true
+			),
+			getItem(
+				"Properties",
+				"/properties",
+				<Icon icon={"mdi:building"} />,
+				[
+					getItem(
+						"Overview",
+						"/app/properties/overview",
+						<Icon icon={"icon-park-outline:view-grid-list"} />
+					),
+					getItem(
+						"Create New Property",
+						"/app/properties/create",
+						<Icon icon={"bi:building-add"} />
+					),
+					getItem(
+						"List of Properties",
+						"/app/properties/all",
+						<Icon icon={"fluent:data-funnel-24-regular"} />,
+						[
+							getItem(
+								"All Properties",
+								"/app/properties",
+								<Icon icon={"bx:buildings"} />
+							),
+							getItem(
+								"New Properties",
+								"/app/properties/all?date=last-7-days",
+								<Icon icon={"ic:outline-new-releases"} />
+							),
+							getItem(
+								"Available Properties",
+								"/app/properties/all?status=available",
+								<Icon icon={"bi:building"} />
+							),
+							getItem(
+								"Sold Properties",
+								"/app/properties/all?status=sold",
+								<Icon icon={"bi:building-check"} />
+							),
+							getItem(
+								"Canceled Properties",
+								"/app/properties/all?status=canceled",
+								<Icon icon={"bi:building-x"} />
+							),
+						]
+					),
+					getItem("Trash", "/properties/trash", <Icon icon={"gg:trash"} />),
+					getItem(
+						"Reports",
+						"/app/properties/reports",
+						<Icon icon={"tabler:report"} />,
+						[]
+					),
+				],
+				true
+			),
+			getItem(
+				"Media",
+				"/media",
+				<Icon icon={"radix-icons:dashboard"} />,
+				[
+					getItem(
+						"Overview",
+						"/app/media/overview",
+						<Icon icon={"icon-park-outline:view-grid-list"} />
+					),
+					getItem(
+						"Create New Media",
+						"/app/media/create",
+						<Icon icon={"radix-icons:dashboard"} />
+					),
+					getItem(
+						"List of Media",
+						"/app/media/all",
+						<Icon icon={"radix-icons:dashboard"} />,
+						[
+							getItem(
+								"All Media",
+								"/app/media/all",
+								<Icon icon={"radix-icons:dashboard"} />
+							),
+							getItem(
+								"Property Media",
+								"/app/media/all?linked_to=property",
+								<Icon icon={"radix-icons:dashboard"} />
+							),
+							getItem(
+								"Lead Media",
+								"/app/media/all?linked_to=lead",
+								<Icon icon={"radix-icons:dashboard"} />
+							),
+						]
+					),
+					getItem(
+						"Trash",
+						"/app/media/trash",
+						<Icon icon={"radix-icons:dashboard"} />
+					),
+					getItem(
+						"Reports",
+						"/media/reports",
+						<Icon icon={"radix-icons:dashboard"} />,
+						[]
+					),
+				],
+				true
+			),
+		],
+	},
+	{
+		type: "group",
+		label: "Organization",
+		children: [
+			getItem(
+				"Employees",
+				"/app/employee",
+				<Icon icon={"radix-icons:dashboard"} />,
+				[
+					getItem(
+						"Create New Employee",
+						"/app/create/employee",
+						<Icon icon={"radix-icons:dashboard"} />
+					),
+					getItem(
+						"List of Employees",
+						"/app/employees/all",
+						<Icon icon={"radix-icons:dashboard"} />,
+						[
+							getItem(
+								"All Employees",
+								"/app/employees",
+								<Icon icon={"radix-icons:dashboard"} />
+							),
+							getItem(
+								"New Employees",
+								"/app/employees/all?join_date<7d",
+								<Icon icon={"radix-icons:dashboard"} />
+							),
+							getItem(
+								"Active Employees",
+								"/app/employees/all?active=true",
+								<Icon icon={"radix-icons:dashboard"} />
+							),
+							getItem(
+								"Suspended Employees",
+								"/app/employees/all?is_active=false",
+								<Icon icon={"radix-icons:dashboard"} />
+							),
+						]
+					),
+					getItem(
+						"Trash",
+						"/app/employees/trash",
+						<Icon icon={"radix-icons:dashboard"} />
+					),
+				]
+			),
+			getItem("Roles", "/app/role", <Icon icon={"radix-icons:dashboard"} />, [
+				getItem(
+					"Create New Role",
+					"/app/create/role",
+					<Icon icon={"radix-icons:dashboard"} />
+				),
+				getItem(
+					"List of Roles",
+					"/app/roles/all",
+					<Icon icon={"radix-icons:dashboard"} />,
+					[
+						getItem(
+							"All Roles",
+							"/app/roles",
+							<Icon icon={"radix-icons:dashboard"} />
+						),
+						getItem(
+							"New Roles",
+							"/app/roles/all?join_date<7d",
+							<Icon icon={"radix-icons:dashboard"} />
+						),
+						getItem(
+							"Active Roles",
+							"/app/roles/all?active=true",
+							<Icon icon={"radix-icons:dashboard"} />
+						),
+						getItem(
+							"Suspended Roles",
+							"/app/roles/all?is_active=false",
+							<Icon icon={"radix-icons:dashboard"} />
+						),
+					]
+				),
+				getItem(
+					"Trash",
+					"/app/roles/trash",
+					<Icon icon={"radix-icons:dashboard"} />
+				),
+			]),
+		],
+	},
 ];
 
 type MenuMode = "inline" | "horizontal" | "vertical";
 
 const Menu: React.FC<{ mode?: MenuMode }> = ({ mode = "inline" }) => {
-  const navigate = useNavigate();
+	const navigate = useNavigate();
+	const location = useLocation();
 
-  return (
-    <>
-      <AntMenu
-        defaultSelectedKeys={["/app"]}
-        mode={mode}
-        items={items}
-        onSelect={(info) => navigate(info.key)}
-        className="relative border-none text-text-light font-semibold  [&_.ant-menu-item-selected]:bg-[#A8FFE4] [&_.ant-menu-item-selected>.ant-menu-title-content]:text-text [&_.ant-menu-item-selected>.ant-menu-item-icon]:text-[#1A946E] [&_.ant-menu-item-icon]:text-xl"
-      />
-    </>
-  );
+	return (
+		<>
+			<AntMenu
+				selectedKeys={[location.pathname]}
+				mode={mode}
+				items={items}
+				onSelect={(info) => navigate(info.key)}
+				className="relative border-none text-text-light font-semibold  [&_.ant-menu-item-selected]:bg-[#A8FFE4] [&_.ant-menu-item-selected>.ant-menu-title-content]:text-text [&_.ant-menu-item-selected>.ant-menu-item-icon]:text-[#1A946E] [&_.ant-menu-item-icon]:text-xl"
+			/>
+		</>
+	);
 };
 
 export default Menu;
--- src/layouts/App/index.tsx
+++ src/layouts/App/index.tsx
@@ -1,25 +1,62 @@
 import useAuth from "@/hooks/useAuth";
+import { Icon } from "@iconify/react";
+import { AppBar, Avatar, Drawer, IconButton, Toolbar } from "@mui/material";
+import { useToggle } from "@tam11a/react-use-hooks";
 import React, { lazy } from "react";
-import { Navigate, Outlet, useLocation } from "react-router-dom";
+import { Link, Navigate, Outlet, useLocation } from "react-router-dom";
 
 const AppDrawer = lazy(() => import("./Drawer"));
 
 const AppLayout: React.FC = () => {
-  const location = useLocation();
+	const location = useLocation();
 
-  const { isLoggedIn } = useAuth();
-  return isLoggedIn ? (
-    <>
-      <div className="bg-background flex flex-row gap-6 p-6 h-screen ">
-        <AppDrawer />
-        <main className="flex-1 bg-background-light overflow-y-auto overflow-x-hidden rounded-md p-4">
-          <Outlet />
-        </main>
-      </div>
-    </>
-  ) : (
-    <Navigate to={`/?to=${location.pathname}`} />
-  );
+	const { state: open, toggleState: onClose, setState } = useToggle(false);
+
+	const { isLoggedIn } = useAuth();
+
+	React.useEffect(() => {
+		setState(false);
+	}, [location.pathname]);
+
+	return isLoggedIn ? (
+		<>
+			<div className="lg:bg-background flex flex-col lg:flex-row lg:gap-6 lg:p-6 h-screen ">
+				<aside className="bg-background-light overflow-hidden rounded-md p-4 flex-[0.40] max-w-[300px] hidden lg:flex flex-col relative">
+					<AppDrawer />
+				</aside>
+				<AppBar className="lg:hidden bg-primary-50">
+					<Toolbar className="justify-between py-1">
+						<Link to={"/app"}>
+							<Avatar
+								src="/assets/logo.png"
+								variant="square"
+								className="h-16 w-auto p-1"
+							/>
+						</Link>
+						<IconButton onClick={onClose}>
+							<Icon icon={"line-md:menu"} />
+						</IconButton>
+						<Drawer
+							open={open}
+							anchor="right"
+							onClose={onClose}
+							PaperProps={{
+								className: "w-[95vw] max-w-[400px]",
+							}}
+						>
+							<AppDrawer />
+						</Drawer>
+					</Toolbar>
+				</AppBar>
+
+				<main className="flex-1 bg-background-light overflow-y-auto overflow-x-hidden lg:rounded-md mt-20 lg:mt-0 p-2 lg:p-4">
+					<Outlet />
+				</main>
+			</div>
+		</>
+	) : (
+		<Navigate to={`/?to=${location.pathname}`} />
+	);
 };
 
 export default AppLayout;
