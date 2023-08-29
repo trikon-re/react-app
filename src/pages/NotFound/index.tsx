bad8af2fbdbd3ef51b36a333247246e4606306a6
Asatter833 <asatter833@gmail.com>
2023-08-30 04:08:00 +0600

    fixes

--- src/pages/NotFound/index.tsx
+++ src/pages/NotFound/index.tsx
@@ -1,7 +1,12 @@
+import { Avatar } from "@mui/material";
 import React from "react";
 
 const NotFound: React.FC = () => {
-	return <div>NotFound</div>;
+  return (
+    <div className="flex items-center justify-center">
+      <Avatar src="/404.svg" className="w-[90%] h-auto max-w-lg" />
+    </div>
+  );
 };
 
 export default NotFound;
