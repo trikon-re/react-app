44705bdf218e6749a71332e8e88cdac56a281343
Asatter833 <asatter833@gmail.com>
2023-08-30 03:39:12 +0600

    fixed some mis issues

--- src/layouts/App/Drawer/index.tsx
+++ src/layouts/App/Drawer/index.tsx
@@ -7,52 +7,52 @@ import { stringAvatar } from "@/utilities/stringAvatar";
 import { Link } from "react-router-dom";
 
 const AppDrawer: React.FC = () => {
-	const { first_name, last_name, role, id, display_picture } = useUser();
-	return (
-		<>
-			<div className="flex-1 overflow-y-auto">
-				<Menu mode="inline" />
-			</div>
-			<div
-				className={
-					"flex flex-row items-center gap-3 justify-between px-3 py-2 "
-				}
-			>
-				<Avatar
-					variant="rounded"
-					src={display_picture}
-					{...stringAvatar(`${first_name} ${last_name}`)}
-					className="w-[47px] h-[47px] rounded-xl"
-				/>
-				<ListItemText
-					primary={`${first_name} ${last_name}`}
-					secondary={
-						role ? (
-							<>
-								{role?.name} <br />
-								{role?.id}
-								{role?.name?.[0]}-{id}
-							</>
-						) : (
-							"No Role Assigned"
-						)
-					}
-					primaryTypographyProps={{
-						noWrap: true,
-						className: "font-bold text-xs",
-					}}
-					secondaryTypographyProps={{
-						noWrap: true,
-						className: "text-[10px] leading-tight font-semibold",
-					}}
-				/>
-				<Link to={"/app/settings"}>
-					<IconButton color="secondary">
-						<Icon icon={"solar:settings-bold-duotone"} />
-					</IconButton>
-				</Link>
-			</div>
-		</>
-	);
+  const { first_name, last_name, role, id, display_picture } = useUser();
+  return (
+    <>
+      <div className="flex-1 overflow-y-auto">
+        <Menu mode="inline" />
+      </div>
+      <div
+        className={
+          "flex flex-row items-center gap-3 justify-between px-3 py-2 "
+        }
+      >
+        <Avatar
+          variant="rounded"
+          src={display_picture}
+          {...stringAvatar(`${first_name} ${last_name}`)}
+          className="w-[56px] h-[56px] rounded-xl"
+        />
+        <ListItemText
+          primary={`${first_name} ${last_name}`}
+          secondary={
+            role ? (
+              <>
+                {role?.name} <br />
+                {role?.id}
+                {role?.name?.[0]}-{id}
+              </>
+            ) : (
+              "No Role Assigned"
+            )
+          }
+          primaryTypographyProps={{
+            noWrap: true,
+            className: "font-bold text-sm",
+          }}
+          secondaryTypographyProps={{
+            noWrap: true,
+            className: "text-xs leading-tight font-semibold",
+          }}
+        />
+        <Link to={"/app/settings"}>
+          <IconButton color="secondary">
+            <Icon icon={"solar:settings-bold-duotone"} />
+          </IconButton>
+        </Link>
+      </div>
+    </>
+  );
 };
 export default AppDrawer;
--- src/layouts/App/index.tsx
+++ src/layouts/App/index.tsx
@@ -8,55 +8,55 @@ import { Link, Navigate, Outlet, useLocation } from "react-router-dom";
 const AppDrawer = lazy(() => import("./Drawer"));
 
 const AppLayout: React.FC = () => {
-	const location = useLocation();
+  const location = useLocation();
 
-	const { state: open, toggleState: onClose, setState } = useToggle(false);
+  const { state: open, toggleState: onClose, setState } = useToggle(false);
 
-	const { isLoggedIn } = useAuth();
+  const { isLoggedIn } = useAuth();
 
-	React.useEffect(() => {
-		setState(false);
-	}, [location.pathname]);
+  React.useEffect(() => {
+    setState(false);
+  }, [location.pathname]);
 
-	return isLoggedIn ? (
-		<>
-			<div className="lg:bg-background flex flex-col lg:flex-row lg:gap-6 lg:p-6 h-screen ">
-				<aside className="bg-background-light overflow-hidden rounded-md p-4 flex-[0.40] max-w-[400px] hidden lg:flex flex-col relative">
-					<AppDrawer />
-				</aside>
-				<AppBar className="lg:hidden bg-primary-50">
-					<Toolbar className="justify-between py-1">
-						<Link to={"/app"}>
-							<Avatar
-								src="/assets/logo.png"
-								variant="square"
-								className="h-16 w-auto p-1"
-							/>
-						</Link>
-						<IconButton onClick={onClose}>
-							<Icon icon={"line-md:menu"} />
-						</IconButton>
-						<Drawer
-							open={open}
-							anchor="right"
-							onClose={onClose}
-							PaperProps={{
-								className: "w-[95vw] max-w-[400px]",
-							}}
-						>
-							<AppDrawer />
-						</Drawer>
-					</Toolbar>
-				</AppBar>
+  return isLoggedIn ? (
+    <>
+      <div className="lg:bg-background flex flex-col lg:flex-row lg:gap-6 lg:p-6 h-screen ">
+        <aside className="bg-background-light overflow-hidden rounded-md p-4 pb-0 flex-[0.40] max-w-[400px] hidden lg:flex flex-col relative">
+          <AppDrawer />
+        </aside>
+        <AppBar className="lg:hidden bg-primary-50">
+          <Toolbar className="justify-between py-1">
+            <Link to={"/app"}>
+              <Avatar
+                src="/assets/logo.png"
+                variant="square"
+                className="h-16 w-auto p-1"
+              />
+            </Link>
+            <IconButton onClick={onClose}>
+              <Icon icon={"line-md:menu"} />
+            </IconButton>
+            <Drawer
+              open={open}
+              anchor="right"
+              onClose={onClose}
+              PaperProps={{
+                className: "w-[95vw] max-w-[400px]",
+              }}
+            >
+              <AppDrawer />
+            </Drawer>
+          </Toolbar>
+        </AppBar>
 
-				<main className="flex-1 bg-background-light overflow-y-auto overflow-x-hidden lg:rounded-md mt-20 lg:mt-0 p-2 lg:p-4">
-					<Outlet />
-				</main>
-			</div>
-		</>
-	) : (
-		<Navigate to={`/?to=${location.pathname}`} />
-	);
+        <main className="flex-1 bg-background-light overflow-y-auto overflow-x-hidden lg:rounded-md mt-20 lg:mt-0 p-2 lg:p-4">
+          <Outlet />
+        </main>
+      </div>
+    </>
+  ) : (
+    <Navigate to={`/?to=${location.pathname}`} />
+  );
 };
 
 export default AppLayout;
