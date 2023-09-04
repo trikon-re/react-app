99d302b5c721d1d1ce18bfb6375c098e3627731e
tam11a <ibrahimsadiktamim@gmail.com>
2023-09-04 17:53:50 +0600

    updated: shit

--- src/pages/Employees/pages/All/index.tsx
+++ src/pages/Employees/pages/All/index.tsx
@@ -6,39 +6,44 @@ import useSearchParamsPaginate from "@/hooks/useSearchParamsPaginate";
 import { Pagination } from "antd";
 
 const Employees: React.FC = () => {
-  const { page, setPage, getQueryParams, limit, setLimit } =
-    useSearchParamsPaginate();
+	const { page, setPage, getQueryParams, limit, setLimit } =
+		useSearchParamsPaginate();
 
-  const { data } = useGetEmployees(getQueryParams());
-  const [employees, setEmployees] = React.useState<any>([]);
+	const { data } = useGetEmployees(getQueryParams());
+	const [employees, setEmployees] = React.useState<any>([]);
 
-  React.useEffect(() => {
-    if (!data) return;
-    setEmployees(data?.data?.data);
-  }, [data]);
+	React.useEffect(() => {
+		if (!data) return;
+		setEmployees(data?.data?.data);
+	}, [data]);
 
-  return (
-    <>
-      <div className="py-2">
-        {employees?.map?.((s: IEmployees) => (
-          <EmployeeCard employee={s} key={s.id} />
-        ))}
+	return (
+		<>
+			<div className="py-2">
+				{employees?.map?.((s: IEmployees) => (
+					<EmployeeCard
+						employee={s}
+						key={s.id}
+					/>
+				))}
 
-        <div className="flex flex-row justify-end">
-          <Pagination
-            total={data?.data?.total}
-            onChange={(p, ps) => {
-              setPage(p);
-              setLimit(ps);
-            }}
-            current={page}
-            pageSize={limit}
-            showSizeChanger
-            pageSizeOptions={[2, 5, 10, 20, 50, 100, 500]}
-          />
-        </div>
-      </div>
-    </>
-  );
+				<div className="flex flex-row justify-end">
+					<Pagination
+						total={data?.data?.total}
+						onChange={(p) => {
+							setPage(p - 1);
+						}}
+						onShowSizeChange={(c, s) => {
+							setLimit(s);
+						}}
+						current={page + 1}
+						pageSize={limit}
+						showSizeChanger
+						pageSizeOptions={[2, 5, 10, 20, 50, 100, 500]}
+					/>
+				</div>
+			</div>
+		</>
+	);
 };
 export default Employees;
