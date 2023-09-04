de3136ba2932f7793fe8eaeb53b698f91f9a81ae
tam11a <ibrahimsadiktamim@gmail.com>
2023-09-04 18:15:28 +0600

    done

--- src/pages/Media/routes/mediaRoutes.ts
+++ src/pages/Media/routes/mediaRoutes.ts
@@ -2,12 +2,21 @@ import { lazy } from "react";
 import { MEDIA_ROUTES } from "./path";
 
 export const mediaRoutes = [
-  {
-    path: MEDIA_ROUTES.ROOT,
-    Component: lazy(() => import("../pages/All")),
-  },
-  {
-    path: MEDIA_ROUTES.NOTFOUND,
-    Component: lazy(() => import("@pages/NotFound")),
-  },
+	{
+		path: MEDIA_ROUTES.ROOT,
+		Component: lazy(() => import("../pages/All")),
+	},
+	{
+		path: MEDIA_ROUTES.OVERVIEW,
+		Component: lazy(() => import("../../Dashboard/pages/Overview")),
+	},
+
+	{
+		path: MEDIA_ROUTES.REPORT,
+		Component: lazy(() => import("../../Dashboard/pages/Overview")),
+	},
+	{
+		path: MEDIA_ROUTES.NOTFOUND,
+		Component: lazy(() => import("@pages/NotFound")),
+	},
 ];
--- src/pages/Properties/routes/propertyRoutes.ts
+++ src/pages/Properties/routes/propertyRoutes.ts
@@ -2,12 +2,21 @@ import { lazy } from "react";
 import { PROPERTY_ROUTES } from "./paths";
 
 export const propertyRoutes = [
-  {
-    path: PROPERTY_ROUTES.ROOT,
-    Component: lazy(() => import("../pages/All")),
-  },
-  {
-    path: PROPERTY_ROUTES.NOTFOUND,
-    Component: lazy(() => import("@pages/NotFound")),
-  },
+	{
+		path: PROPERTY_ROUTES.ROOT,
+		Component: lazy(() => import("../pages/All")),
+	},
+	{
+		path: PROPERTY_ROUTES.OVERVIEW,
+		Component: lazy(() => import("../../Dashboard/pages/Overview")),
+	},
+
+	{
+		path: PROPERTY_ROUTES.REPORT,
+		Component: lazy(() => import("../../Dashboard/pages/Overview")),
+	},
+	{
+		path: PROPERTY_ROUTES.NOTFOUND,
+		Component: lazy(() => import("@pages/NotFound")),
+	},
 ];
